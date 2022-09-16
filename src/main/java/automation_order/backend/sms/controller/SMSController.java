4585a5b4a0e0cd1570f352b733b1784eaf33600b
magantus@googlemail.com <Extrem93!>
2022-09-16 14:48:20 +0200

    update

--- src/main/java/automation_order/backend/sms/controller/SMSController.java
+++ src/main/java/automation_order/backend/sms/controller/SMSController.java
@@ -3,9 +3,14 @@ package automation_order.backend.sms.controller;
 import automation_order.backend.sms.model.SMS;
 import automation_order.backend.sms.service.SMSService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.ConnectException;
 
 
 @RestController
@@ -21,11 +26,12 @@ public class SMSController {
 
 
     @PostMapping("/send")
-    public String senSMS() {
-        SMS sms = new SMS();
-        sms.setPhone("+4915734696774");
-        sms.setMsg("Test Message from Spring Boot");
-        return smsService.sendMsg(sms);
+    public ResponseEntity<String> senSMS(@RequestBody SMS sms) {
+        try{
+            return new ResponseEntity<String>(smsService.sendMsg(sms), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.REQUEST_TIMEOUT);
+        }
     }
 
 }
