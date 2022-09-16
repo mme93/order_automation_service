006748e2ad68286bc6b2b24602b22a1f30da7cac
magantus@googlemail.com <Extrem93!>
2022-09-16 13:31:11 +0200

    update

---
+++ src/main/java/automation_order/backend/sms/model/SMS.java
@@ -0,0 +1,17 @@
+package automation_order.backend.sms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class SMS {
+
+    private String SMS;
+    private String phoneNumber;
+
+}
---
+++ src/main/java/automation_order/backend/sms/service/SMSService.java
@@ -0,0 +1,7 @@
+package automation_order.backend.sms.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SMSService {
+}
