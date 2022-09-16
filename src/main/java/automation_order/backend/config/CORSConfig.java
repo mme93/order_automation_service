cb4baea6c83e97bb498ec7f105178a9839d0a482
magantus@googlemail.com <Extrem93!>
2022-09-16 15:08:35 +0200

    add Cors Config

---
+++ src/main/java/automation_order/backend/config/CORSConfig.java
@@ -0,0 +1,17 @@
+package automation_order.backend.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CORSConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .maxAge(1800L);
+    }
+}
