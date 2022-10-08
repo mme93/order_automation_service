f37775c7350afa9330fa999fa52bd51db8900a8e
magantus@googlemail.com <Extrem93!>
2022-10-08 23:32:11 +0200

    update

--- src/main/java/automation_order/backend/order/controller/OrderController.java
+++ src/main/java/automation_order/backend/order/controller/OrderController.java
@@ -1,9 +1,9 @@
 package automation_order.backend.order.controller;
 
 import automation_order.backend.order.model.dto.OrderDto;
-import automation_order.backend.order.model.dto.TodoDto;
 import automation_order.backend.order.service.OrderService;
-import org.json.JSONObject;
+import automation_order.backend.sms.model.SMS;
+import automation_order.backend.sms.service.SMSService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,16 +16,20 @@ import java.util.List;
 public class OrderController {
 
     private final OrderService orderService;
+    private final SMSService smsService;
 
     @Autowired
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, SMSService smsService) {
         this.orderService = orderService;
+        this.smsService = smsService;
     }
 
 
     @PostMapping("/create")
     public ResponseEntity<String> createOrder(@RequestBody OrderDto orderDto) {
-        return new ResponseEntity(this.orderService.createOrder(orderDto),HttpStatus.OK);
+        String password=this.orderService.createOrder(orderDto);
+        SMS sms = this.smsService.createSMS(this.orderService.getOrderByPassword(password));
+        return new ResponseEntity(this.smsService.sendMsg(sms),HttpStatus.OK);
     }
 
     @GetMapping("/all")
